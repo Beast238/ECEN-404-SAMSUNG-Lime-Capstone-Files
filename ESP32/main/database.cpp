167ecc0fbeecaa5b9a1f32f537c903aa6e2bd8f2
atenfyr <hello@atenfyr.com>
2025-10-17 19:30:51 -0500

    decrease http req time to 2s

--- ESP32/main/database.cpp
+++ ESP32/main/database.cpp
@@ -785,7 +785,7 @@ vTaskDelay(pdMS_TO_TICKS(5000));//5 sec delay until request starts.
 
 
         
-       vTaskDelay(pdMS_TO_TICKS(5000));//sets specific updates if needed to. CHANGE THIS IF NEEDED DURING INTEGRATION!!!!
+       vTaskDelay(pdMS_TO_TICKS(2000));//sets specific updates if needed to. CHANGE THIS IF NEEDED DURING INTEGRATION!!!!
 
     //}
 
